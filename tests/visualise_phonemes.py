60c4ece7e4eeb1e4670e54009265f641d18fbc4b
Charlie <cxh162@student.bham.ac.uk>
2025-01-09 14:03:15 +0000

    finished phoneme assignment algorithm. tested on other transcriptions which have small issues

--- tests/visualise_phonemes.py
+++ tests/visualise_phonemes.py
@@ -86,6 +86,7 @@ class VisualisePhonemes:
             word_phoneme_regions = []
 
             for j in range(len(self.phoneme_regions)):
+                #print(self.transcription_array[i][0], self.phoneme_regions[j][0])
                 if (# if start time is within the phoneme region
                     ((self.transcription_array[i][0] <= self.phoneme_regions[j][0]) and 
                     (self.transcription_array[i][1] > self.phoneme_regions[j][0])) or
@@ -118,13 +119,119 @@ class VisualisePhonemes:
             # more regions than phonemes
             elif (len(word_phonemes) < len(available_phoneme_regions)):
                 print("2")
-                assigned_indexes = self.calculate_strongest_phonemes(len(word_phonemes), available_phoneme_regions)
-                for j in range(len(assigned_indexes)):
-                    self.phoneme_regions[assigned_indexes[j]][2] = word_phonemes[j]
+                # if a region is below a fraction of the average silent decibels
+                # treat it as a silent mouth movement and merge with the next region
+
+                SILENT_FRACTION = 0.9
+
+                region_decibels = []
+
+                total_db_length = len(self.total_decibels)
+
+                # get the average decibels per time step in each phoneme region
+                for j in range(0, len(available_phoneme_regions)):
+                    start_fraction = int((available_phoneme_regions[j][0]/self.audio_length)*total_db_length)
+                    end_fraction = int((available_phoneme_regions[j][1]/self.audio_length)*total_db_length)
+                    #print(start_fraction, end_fraction)
+                    region_decibels.append([
+                        np.average(self.average_decibels[start_fraction:end_fraction]), 
+                        int(available_phoneme_regions[j][3])])
+
+                available_phoneme_regions_len = len(available_phoneme_regions)
+                phoneme_counter = 0
+                # merge regions of silence with next loud region
+                for j in range(0, available_phoneme_regions_len):
+
+                    current_index = available_phoneme_regions[j][3]
+
+                    print(region_decibels[j][0], SILENT_FRACTION*self.silent_average)
+                    print(region_decibels[j][0] < SILENT_FRACTION*self.silent_average)
+
+                    # if the average decibels of a region is below a threshold
+                    if (region_decibels[j][0] < SILENT_FRACTION*self.silent_average):
+                        print("silent region")
+                        # if there aren't the same number of available regions left 
+                        # and phonemes still to assign
+                        if ((len(word_phonemes) - phoneme_counter) < (available_phoneme_regions_len - j)):
+                            # if we aren't at the last region in the phrase
+                            if (self.phoneme_regions[current_index][3] != len(self.phoneme_regions)-1):
+                                #print(self.phoneme_regions[current_index+1][0], self.phoneme_regions[current_index][0])
+                                # set the second region's start point to be the start of the silent section
+                                self.phoneme_regions[current_index+1][0] = self.phoneme_regions[current_index][0]
+                            # if we are at the last region in the phrase
+                            else:
+                                # set the previous region's end point to be the end of the silent section
+                                self.phoneme_regions[current_index-1][1] = self.phoneme_regions[current_index][1]
+
+                            # delete the silent phoneme region
+                            del self.phoneme_regions[current_index]
+                            # delete the silent phoneme region from the array of available phonemes
+                            # del available_phoneme_regions[j]
+                            print(self.phoneme_regions)
+
+                            self.add_index_to_array(self.phoneme_regions)
+                        # if we are on the last X regions with X phonemes still to assign
+                        else:
+                            self.phoneme_regions[current_index][2] = word_phonemes[phoneme_counter]
+                            phoneme_counter += 1
+                    # if the region is not silent
+                    else:
+                        # if we haven't assigned all phonemes available for the current word
+                        if (phoneme_counter < len(word_phonemes)):
+                            #print(word_phonemes[phoneme_counter])
+                            # assign the current region to the next phoneme
+                            self.phoneme_regions[current_index][2] = word_phonemes[phoneme_counter]
+                            # increment the phoneme counter
+                            # phoneme_counter < available_phoneme_regions_len
+                            phoneme_counter += 1
+
+                # assigned_indexes = self.calculate_strongest_phonemes(len(word_phonemes), available_phoneme_regions)
+                # for j in range(len(assigned_indexes)):
+                #     self.phoneme_regions[assigned_indexes[j]][2] = word_phonemes[j]
             else:
-                print("ERROR")
+                # if we are one phoneme region short and
+                # the first region is already taken
+                # split it
+                if (len(word_phoneme_regions)-1 == len(available_phoneme_regions)):
+                    if (word_phoneme_regions[0][2] != ''):
+                        # split at the time stamp for the word
+                        section_to_split = word_phoneme_regions[0]
+                        split_time = self.transcription_array[i][0]
+
+                        # if the time stamp does split the already assigned phoneme region
+                        if (split_time > section_to_split[0] and split_time <= section_to_split[1]):
+                            # create new region and assign phoneme to it
+                            new_region = [split_time, section_to_split[1], word_phonemes[0], -1]
+                            # modify the end time of the original region to be the split time
+                            self.phoneme_regions[section_to_split[3]][1] = split_time
+                            # remove the now assigned phoneme from the list
+                            word_phonemes = word_phonemes[1:]
+                            # insert new section after the split section
+                            self.phoneme_regions.insert(section_to_split[3]+1, new_region)
+                            print("inserted")
+                            # re-index all items in the array
+                            self.add_index_to_array(self.phoneme_regions)
+                            print("indexes added")
+                            
+
+                            for j in range(len(available_phoneme_regions)):
+                                self.phoneme_regions[available_phoneme_regions[j][3]][2] = word_phonemes[j]
+
+                            print(self.phoneme_regions)
+                        else:
+                            print("split time not in region")
+                    else:
+                        print("region not assigned")
+                else:
+                    print("multiple regions already assigned")
+                #print("ERROR")
         print(self.phoneme_regions)
         return
+
+
+    def add_index_to_array(self, array):
+        for i, sub_array in enumerate(array):
+            sub_array[3] = i
     
     def calculate_strongest_phonemes(self, n, available_phoneme_regions):
         # sort phoneme regions by loudest and take the top n
@@ -133,14 +240,16 @@ class VisualisePhonemes:
 
         total_db_length = len(self.total_decibels)
 
+        # get the average decibels per time step in each phoneme region
         for i in range(0, len(available_phoneme_regions)):
             start_fraction = int((available_phoneme_regions[i][0]/self.audio_length)*total_db_length)
             end_fraction = int((available_phoneme_regions[i][1]/self.audio_length)*total_db_length)
             print(start_fraction, end_fraction)
             decibels.append([
-                np.sum(self.total_decibels[start_fraction:end_fraction]), 
+                np.mean(self.total_decibels[start_fraction:end_fraction]), 
                 int(available_phoneme_regions[i][3])])
-        #print(decibels)
+        
+        print(decibels)
         sorted_decibels = sorted(decibels, key=lambda x: x[0], reverse=True)[:n]
         sorted_decibels = [item[1] for item in sorted_decibels]
         sorted_decibels = sorted(sorted_decibels)
@@ -194,11 +303,14 @@ class VisualisePhonemes:
                 self.inflection_indexes[i], 
                 self.inflection_indexes[i-1]], 
                 [0, 0, self.sample_rate/2, self.sample_rate/2], 
-                color='blue', alpha=0.4, zorder=8)
+                color='blue', alpha=0.2, zorder=8)
 
         #ax.scatter(cent_times, centroids, zorder=5, color='red')
 
         ax.vlines(self.inflection_indexes, colors='orange', ymin=0, ymax=self.sample_rate/2)
+
+        ax.vlines([pair[0] for pair in self.transcription_array], 
+        colors='black', ymin=0, ymax=self.sample_rate/2)
 
         ax.scatter(self.inflection_indexes, self.inflection_values, zorder=7, color='green')
 
@@ -315,13 +427,20 @@ class VisualisePhonemes:
         pre_sil = np.array(self.average_decibels[:sil_end_index])
         post_sil = np.array(self.average_decibels[sil_start_index:])
         
-        self.silent_average = np.mean(np.concat((pre_sil, post_sil)))
+        # silence from before and after speech
+        #self.silent_average = np.mean(np.concat((pre_sil, post_sil)))
+        # silence from after speech
+        self.silent_average = np.mean(post_sil)
+        print(self.silent_average)
 
     def create_xticks(self):
         self.time_labels = [pair[0] for pair in self.transcription_array]
         self.word_labels = [pair[2] for pair in self.transcription_array]
 
-FILE_NAME = 'swwp2s_high.wav'
+#FILE_NAME = 'swwp2s_high.wav'
+FILE_NAME = 'C:/Users/charl/OneDrive/ASUS/Documents/CS3/Project/dataset/s23_50kHz/s23/bbad1s.wav'
+#TRANS_FILE_NAME = 'swwp2s.align.txt'
+TRANS_FILE_NAME = 'C:/Users/charl/OneDrive/ASUS/Documents/CS3/Project/dataset/s23/align/bbad1s.align'
 FRAME_RATE = 25
 nfft = 128
 noverlap = nfft // 2
@@ -329,7 +448,7 @@ noverlap = nfft // 2
 y, sr = librosa.load(FILE_NAME)
 
 PhonLib = PhonemeLibrary()
-transcription_array = PhonLib.create_transcription_array('swwp2s.align.txt', 25)
+transcription_array = PhonLib.create_transcription_array(TRANS_FILE_NAME, 25)
 
 VisPhon = VisualisePhonemes(FILE_NAME, 25, transcription_array)
 
