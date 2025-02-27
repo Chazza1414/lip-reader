e3c2f53c7d1ae967bb616d3fe7c8952680d55bfa
C Hill <charlieljhill@gmail.com>
2025-02-27 12:36:01 +0000

    added categorical accuracy and model evaluation

--- training/train.py
+++ training/train.py
@@ -1,6 +1,7 @@
 from keras.optimizers import Adam
 from keras.losses import CategoricalCrossentropy
 from keras.callbacks import TensorBoard
+from keras.metrics import CategoricalAccuracy
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -15,29 +16,37 @@ np.random.seed(55)
 
 def train(run_name, start_epoch, stop_epoch, img_c, img_w, img_h, frames_n, minibatch_size, dataset_path):
 
-    lipreader_generator = Generator(minibatch_size=minibatch_size,
-                                img_c=img_c, img_w=img_w, img_h=img_h, frames_n=frames_n,
-                                start_epoch=start_epoch, dataset_path=dataset_path).build()
+    lipreader_generator = Generator(minibatch_size=minibatch_size, start_epoch=start_epoch, dataset_path=dataset_path).build()
 
     lipreader = LipReader(img_c=img_c, img_w=img_w, img_h=img_h, frames_n=frames_n, output_size=lipreader_generator.output_size)
     lipreader.summary()
 
     adam = Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     loss = CategoricalCrossentropy(reduction="sum_over_batch_size", name="CCE")
+    accuracy = CategoricalAccuracy(name="categorical_accuracy")
 
     lipreader.model.compile(optimizer=adam, loss=loss)
 
     tensorboard = TensorBoard(log_dir=os.path.join(MODEL_LOG_LOCATION, run_name), histogram_freq=1, write_images=True, embeddings_freq=1)
     
+    print("training " + str(run_name))
     lipreader.model.fit(x=LockedIterator(lipreader_generator.next_train()),
                         steps_per_epoch=lipreader_generator.default_training_steps, epochs=stop_epoch,
-                        validation_data=LockedIterator(lipreader_generator.next_val()), validation_steps=lipreader_generator.default_validation_steps,
+                        validation_data=LockedIterator(lipreader_generator.next_validate()), validation_steps=lipreader_generator.default_validation_steps,
                         callbacks=[tensorboard], 
+                        metrics=[accuracy],
                         initial_epoch=start_epoch, 
                         verbose=1,
                         max_queue_size=5,
                         workers=2)
-    print("finished training " + str(run_name))
+    
+    print("evaluating")
+    evaluation_loss, evaluation_accuracy = lipreader.model.evaluate(x=LockedIterator(lipreader_generator.next_evaluate()), 
+                                                                    verbose=1, callbacks=[tensorboard], metrics=[accuracy])
+    
+    print(evaluation_loss)
+    print(evaluation_accuracy)
+
     #  to get rid of the warning from absl, save file as .h5
     lipreader.model.save(Path(MODEL_SAVE_LOCATION) / run_name)
 
